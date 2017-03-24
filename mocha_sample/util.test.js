05a4564235d9e9ccaed8c59c6b6c72d69a13f529
Rami Abdelwahed <rami.abdelwahed@yahoo.com>
2017-03-24 15:22:34 +1100

    Added a sample test for an async method.

--- mocha_sample/util.test.js
+++ mocha_sample/util.test.js
@@ -1,15 +1,23 @@
 const util = require('./util/util');
 const expect = require('expect');
 
-it("Should add 2 numbers", () => {
-    expect(util.add(1, 2)).toBe(3);
+it("should add 2 numbers", () => {
+    expect(util.add(1, 2)).toBe(3).toBeA('number');
 });
 
-it("Should update the user object with first/last names", () => {
+it("should update the user object with first/last names", () => {
     var user = {
         id: 1,
         age: 38
     };
     util.updateUser(user, 'Rami Abdelwahed');
     expect(user).toInclude({firstName: 'Rami', lastName: 'Abdelwahed'});
+});
+
+
+it("should square a number", (done) => {
+    util.square(2, (result) => {
+        expect(result).toBeA('number').toBe(4);
+        done();
+    });
 });
--- mocha_sample/util/util.js
+++ mocha_sample/util/util.js
@@ -1,5 +1,11 @@
 var add = (a, b) => a + b;
 
+var square = (x, callback) => {
+    setTimeout(() => {
+        callback(x * x);
+    }, 1000);
+} 
+
 var updateUser = (user, fullName) => {
     var nameParts =  fullName.split(' ');
     user.firstName = nameParts[0];
@@ -8,5 +14,6 @@ var updateUser = (user, fullName) => {
 
 module.exports = {
     add,
+    square,
     updateUser
 };
